71b38d9179ab05828084c90c7d3a69b9917a69b7
G UDAY KUMAR <156932642+G-uday-kumar@users.noreply.github.com>
2026-03-29 16:25:43 +0530

    Add files via upload

---
+++ eatfit_with_js.js
@@ -0,0 +1,140 @@
+let nav = document.getElementById("nav");
+nav.style.display = "flex";
+nav.style.justifyContent = "space-around";
+nav.style.height = "50px";
+nav.style.borderBottom = "solid black 2px";
+nav.style.alignItems = "center";
+nav.style.fontSize = "30px";
+nav.style.borderRadius = "5px";
+
+let info = document.getElementById("home");
+info.style.textDecoration = "none";
+info.style.padding = "30px";
+info.style.color = "black";
+let eat = document.getElementById("eat");
+eat.style.textDecoration = "none";
+eat.style.padding = "30px";
+eat.style.color = "black";
+let visit = document.getElementById("visit");
+visit.style.textDecoration = "none";
+visit.style.padding = "30px";
+visit.style.color = "black";
+
+let log = document.getElementById("name");
+log.style.display = "flex";
+log.style.alignItems = "center";
+log.style.padding = "20px";
+log.style.gap = "0px";
+log.style.marginBottom = "30px";
+log.style.marginLeft = "600px";
+log.style.blockSize = "25%";
+
+
+// let img=document.getElementById("img")
+// img.style.width="20%"
+
+let logoo = document.getElementById("log");
+logoo.style.alignItems = "center";
+
+let soci = document.getElementById("Soci");
+soci.style.textDecoration = "none";
+soci.style.gap = "30px";
+soci.style.color = "black";
+
+let subnav = document.getElementById("subnav");
+subnav.style.color = "black";
+subnav.style.marginBottom = "30px";
+subnav.style.alignItems = "center";
+
+let a1 = document.getElementById("a1");
+a1.style.textDecoration = "none";
+a1.style.marginLeft = "500px";
+a1.style.color = "black";
+a1.style.fontSize = "25px";
+
+let a2 = document.getElementById("a2");
+a2.style.textDecoration = "none";
+a2.style.marginLeft = "30px";
+a2.style.color = "black";
+a2.style.fontSize = "25px";
+
+let a3 = document.getElementById("a3");
+a3.style.textDecoration = "none";
+a3.style.marginLeft = "30px";
+a3.style.color = "black";
+a3.style.fontSize = "25px";
+
+let a4 = document.getElementById("a4");
+a4.style.textDecoration = "none";
+a4.style.marginLeft = "30px";
+a4.style.color = "black";
+a4.style.fontSize = "25px";
+
+let footer = document.getElementById("footer");
+footer.style.backgroundColor = "blue";
+footer.style.justifyContent = "center";
+footer.style.color="white"
+
+let foot = document.getElementById("foot");
+foot.style.backgroundColor = "blue";
+foot.style.border= "1px solid #fcfafa";
+foot.style.textAlign= "center";
+
+
+
+let follow = document.getElementById("follow");
+follow.style.backgroundColor = "blue";
+follow.style.border= "1px solid #fcfafa";
+follow.style.textAlign= "center";
+
+let l1=document.getElementById("left")
+// left.style.alignItems="center"
+// l1.style.textAlign= "center";
+l1.style.color="white"
+
+
+
+let cards=document.getElementById("cards")
+cards.style.marginTop= "20px";
+cards.style.display= "flex";
+cards.style.flexWrap="wrap";
+cards.style.gap= "20px";
+cards.style.justifyContent=" space-between";
+
+
+let card1=document.getElementById("card1")
+card1.style.border = "solid black 2px";
+card1.style.width= "30%";
+card1.style.borderRadius = "5px";
+
+
+let card2=document.getElementById("card2")
+card2.style.border = "solid black 2px";
+card2.style.width= "30%";
+card2.style.borderRadius = "5px";
+
+let card3=document.getElementById("card3")
+card3.style.border = "solid black 2px";
+card3.style.width= "30%";
+card3.style.borderRadius = "5px";
+
+let card4=document.getElementById("card4")
+card4.style.border = "solid black 2px";
+card4.style.width= "30%";
+card4.style.borderRadius = "5px";
+
+let card5=document.getElementById("card5")
+card5.style.border = "solid black 2px";
+card5.style.width= "30%";
+card5.style.borderRadius = "5px";
+
+let card6=document.getElementById("card6")
+card6.style.border = "solid black 2px";
+card6.style.width= "30%";
+card6.style.borderRadius = "5px";
+
+let p=document.getElementById("cards.p")
+p.style.textAlign="justify"
+p.style.fontSize="15px"
+p.style.fontFamily="Calibri, sans-serif"
+    
